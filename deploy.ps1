7d7ebe1ddd4266fe106577ab0937757f15c3e44e
jhjortiz <jhjortiz@bancolombia.com.co>
2024-03-15 15:44:13 -0500

    Scripts folder create for better files management

--- deploy.ps1
+++ deploy.ps1
@@ -8,21 +8,15 @@ python -m venv $env_name
 .\dynamodb_env\Scripts\Activate.ps1
 
 Write-Host "`nInstalling Python dependencies`n"
-pip install -r requirements.txt
+pip install -r .\scripts\requirements.txt
 
 Write-Host "`nDeploying local DynamoDB database`n" 
 podman compose up -d --build --no-recreate --remove-orphans
 
-Write-Host "`nCreating Table`n"
-python create_table.py $local_endpoint $table_name
+Write-Host "`nExecuting Python scripts`n"
+python .\scripts\create_table.py $local_endpoint $table_name
+python .\scripts\put_items.py $local_endpoint $table_name
 
-Write-Host "`nAdding data into $table_name table`n"
-python put_items.py $local_endpoint $table_name
-
-Write-Host "`nTesting local DynamoDB connection`n"
-aws dynamodb describe-table `
-    --endpoint-url $local_endpoint `
-    --table-name $table_name | findstr TableStatus
-    
+Write-Host "`nTesting local DynamoDB connection`n" 
 aws dynamodb list-tables --endpoint-url $local_endpoint
 aws dynamodb scan --endpoint-url $local_endpoint --table-name $table_name
